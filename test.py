7e102202365b87330b554906a6dc9e87c05b9a9c
Lenny Schwarz <lennyschwarz06@gmail.com>
2023-09-07 19:46:44 +0100

    audio

--- test.py
+++ test.py
@@ -1,1 +1,39 @@
-print("Hello")
+import pyaudio
+import wave
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER
+
+)
+
+print("Start recording")
+
+seconds = 5
+frames = []
+
+for i in range(0, int(RATE/FRAMES_PER_BUFFER*seconds)):
+    data = stream.read(FRAMES_PER_BUFFER)
+    frames.append(data)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
+obj = wave.open("output.wav", "wb")
+obj.setnchannels(CHANNELS)
+obj.setsampwidth(p.get_sample_size) 
+obj.setframerate(RATE)
+obj.writeframes(b"".join(frames))
+obj.close()
