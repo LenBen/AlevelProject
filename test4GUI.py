f8a1a9a3386a3174a4c8a1e53e2e3ef59fb1c106
Lenny <lennyschwarz06@gmail.com>
2023-09-13 21:02:29 +0100

    GUI shenanigans

--- test4GUI.py
+++ test4GUI.py
@@ -5,10 +5,10 @@ app = QApplication([])
 
 window = QWidget()
 window.setWindowTitle("PyQt App")
-window.setGeometry(200, 200, 300, 100)
+window.setGeometry(200, 300, 350, 100)
 helloMsg = QLabel("<h1>Hello, Worlgd!</h1>", parent=window)
 helloMsg.move(60, 15)
-towMsg = QLabel("<p>this is a final thing it works on windows but not on linux i hate it</p>", parent=window)
+towMsg = QLabel("<p>this is a final thing it works on windows</p><p>but not on linux i hate it</p>", parent=window)
 towMsg.move(30, 70)
 
 window.show()
