dc23ed1dfd87e692f123d2646e5abb2f13524333
Lenny <lennyschwarz06@gmail.com>
2023-09-13 13:24:55 +0100

    Added more text if I see this onlinux I do rejoice

--- test4GUI.py
+++ test4GUI.py
@@ -5,7 +5,7 @@ app = QApplication([])
 
 window = QWidget()
 window.setWindowTitle("PyQt App")
-window.setGeometry(100, 100, 280, 80)
+window.setGeometry(200, 200, 300, 100)
 helloMsg = QLabel("<h1>Hello, Worlgd!</h1>", parent=window)
 helloMsg.move(60, 15)
 towMsg = QLabel("<p>this is a final thing it works on windows but not on linux i hate it</p>", parent=window)
