bff984f909afc5298bd7d706ad6a77006963bcc1
Lenny <lennyschwarz06@gmail.com>
2023-09-14 10:33:54 +0100

    more tests for the GUI, thats why its called test

--- test4GUI.py
+++ test4GUI.py
@@ -1,15 +1,40 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from PyQt6.QtWidgets import (
+    QApplication, 
+    QFormLayout,
+    QLineEdit,
+    QLabel, 
+    QWidget,
+    QGridLayout,
+    QFormLayout,
+    QPushButton
+    )
 
 app = QApplication([])
 
 window = QWidget()
-window.setWindowTitle("PyQt App")
-window.setGeometry(200, 300, 350, 100)
-helloMsg = QLabel("<h1>Hello, Worlgd!</h1>", parent=window)
-helloMsg.move(60, 15)
-towMsg = QLabel("<p>this is a final thing it works on windows</p><p>but not on linux i hate it</p>", parent=window)
-towMsg.move(30, 70)
+window.setWindowTitle("QH Box Layouts")
+
+# layout = QGridLayout()
+# layout.addWidget(QPushButton("(0,0)"), 0, 0)
+# layout.addWidget(QPushButton("Center 01"), 0, 1)
+# layout.addWidget(QPushButton("Bottom10"), 1, 0)
+# layout.addWidget(QPushButton("1,1"), 1, 1)
+# layout.addWidget(QPushButton("5,5"),5 ,5)
+# window.setLayout(layout)
+
+layout = QFormLayout()
+layout.addRow("Name:", QLineEdit())
+layout.addRow("Age:", QLineEdit())
+layout.addRow("Job:", QLineEdit())
+layout.addRow("Hobbies:", QLineEdit())
+window.setLayout(layout)
+
+# window.setGeometry(200, 300, 350, 100)
+# helloMsg = QLabel("<h1>Hello, Worlgd!</h1>", parent=window)
+# helloMsg.move(60, 15)
+# towMsg = QLabel("<p>this is a final thing it works on windows</p><p>but not on linux i hate it</p>", parent=window)
+# towMsg.move(30, 70)
 
 window.show()
 
