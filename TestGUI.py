ebfd6473b339a284abe1f02f1a4e822e590f7541
LenBen12345 <lennyschwarz06@gmail.com>
2023-09-19 21:28:06 +0100

    removed teh old audio functions

--- TestGUI.py
+++ TestGUI.py
@@ -70,39 +70,6 @@ class TestGUIWIndow(QMainWindow):
          self.generalLayout.addLayout(buttonsLayout)
          self.button2.clicked.connect(audio.PlayMusic)
 
-# def record():
-     
-#      p = pyaudio.PyAudio()
-#      stream = p.open(rate=RATE,
-#                 format=FORMAT,
-#                 channels=CHANNELS,
-#                 input=True,
-#                 frames_per_buffer=CHUNK)
-#      frames = []
-
-#      print("recording start **")
-
-#      for i in range(0, int(RATE / CHUNK * record_audio)):
-#         data = stream.read(CHUNK)
-#         frames.append(data)
-
-#      print("** recording ended")
-
-#      stream.stop_stream()
-#      stream.close()
-#      p.terminate()
-
-#      wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-#      wf.setnchannels(CHANNELS)
-#      wf.setsampwidth(p.get_sample_size(FORMAT))
-#      wf.setframerate(RATE)
-#      wf.writeframes(b''.join(frames))
-#      wf.close()
-
-# def PlayMusic():
-#     wav = vlc.MediaPlayer("voice.wav")
-#     wav.play()
-
 def main():
       App = QApplication([])
       Window = TestGUIWIndow()
