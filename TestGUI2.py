9f332c588730b952662730e06a5afcf603e32b98
LenBen12345 <lennyschwarz06@gmail.com>
2023-10-01 18:12:49 +0100

    Added comments

--- TestGUI2.py
+++ TestGUI2.py
@@ -53,20 +53,12 @@ class MainWindow(QWidget):
         self._setUpMainWindow()
         self.show()
     
-    def _setUpMainWindow(self):
-        # title_label = QLabel(self)
-        # title_label.setText("Record and Play audio")
-        # title_label.setFont(QFont("Arial", 20))
-        # title_label.move(WINDOW_SIZE//4,30)
+    def _setUpMainWindow(self): # Calls the functions to create labels, line edits and buttons
         self._createButtons()
         self._createLineEditTime()
-        self._setUpLabels()
+        self._createLabels()
 
-        # bottom_label = QLabel(self)
-        # bottom_label.setText("Like a digital Cassette recorder/player!")
-        # bottom_label.move(15,WINDOW_SIZE-20)
-
-    def _setUpLabels(self):
+    def _createLabels(self): # creates labels
         title_label = QLabel(self)
         title_label.setText("Record and Play audio")
         title_label.setFont(QFont("Arial", 20))
@@ -80,7 +72,7 @@ class MainWindow(QWidget):
         self.time_label.move(80,230)
         self.time_label.setText(f"The record length is set to {audio.getRecordTime()} seconds")
     
-    def _createButtons(self):
+    def _createButtons(self): # creates buttons
         self.buttonR = QPushButton("Record", self)
         self.buttonR.move(80,110)
         self.buttonR.setFixedSize(BUTTON_SIZE,BUTTON_SIZE)
@@ -91,8 +83,13 @@ class MainWindow(QWidget):
         self.buttonP.setFixedSize(BUTTON_SIZE,BUTTON_SIZE)
         self.buttonP.clicked.connect(audio.PlayMusic)
 
+        self.buttonC = QPushButton("Enter",self)
+        self.buttonC.move(300,208)
+        self.buttonC.clicked.connect(self._acceptTime)
+        self.buttonC.setDisabled(True)
 
-    def _createLineEditTime(self):
+
+    def _createLineEditTime(self): # creates the Line
         QLabel("Please enter the recording length below (seconds).",self).move(80,190)
 
         self.time_edit = QLineEdit(self)
@@ -100,32 +97,21 @@ class MainWindow(QWidget):
         self.time_edit.move(80,210)
         self.time_edit.textChanged.connect(self._checkIfNotEmpty)
 
-        self.buttonC = QPushButton("Enter",self)
-        self.buttonC.move(300,208)
-        self.buttonC.clicked.connect(self._acceptTime)
-        self.buttonC.setDisabled(True)
-
-        # self.time_label = QLabel(self)
-        # self.time_label.move(80,230)
     
     def _acceptTime(self):
-            # print(time)
-            time = self.time_edit.text()
-            try:
-                t = audio.setRecordTime(time)
-            except:
-                print("No inputty")
-            if t == 55:
+            t = audio.setRecordTime(self.time_edit.text()) # passes the value of the line edit through to the setter of record_audio
+
+            if t == 55: # returns an error message if the data is incorrect
                 QMessageBox.critical(self,
                                       "Character Error",
                                       """<p>The characters inputted are invalid</p>
                                       <p>Please only input numbers</p>""",
                                       QMessageBox.StandardButton.Ok)
-            if t == 0:
+            if t == 0: # if the data is correct it updates the text
                 self.time_label.setText(f"The record length is set to {audio.getRecordTime()} seconds")
 
     
-    def _checkIfNotEmpty(self, text):
+    def _checkIfNotEmpty(self, text): # checks if the line edit is empty. If so the submit button is disabled
         if text:
             self.buttonC.setDisabled(False)
         elif not text:
