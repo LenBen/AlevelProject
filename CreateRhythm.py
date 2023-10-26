c5f47a14c4e82bbd144b9b39e20c1c953d8f256b
LenBen12345 <lennyschwarz06@gmail.com>
2023-10-26 14:09:42 +0100

    Made it so that the time sig is removed

--- CreateRhythm.py
+++ CreateRhythm.py
@@ -1,11 +1,13 @@
 
 class CreateRhythm:
     def __init__(self) -> None:
-        self.rhythmArray = []
+        self.rhythmList = []
+        self.timeSig = ""
 
-    def GetMusic(self):
+    def GetMusic(self) -> None:
         with open("O_Output/music.txt", "r") as notes:
             w = notes.read()
+            w = self._getTimeSig(w)
             letter = False
             for i in w:
                 if not(ord(i) > 46 and ord(i) < 58):
@@ -13,19 +15,41 @@ class CreateRhythm:
                 elif letter:
                     letter = False
                 else:
-                    self.rhythmArray.append(i)
-            self.rhythmArray.reverse()
+                    self.rhythmList.append(i)
+            self.rhythmList.reverse()
             temp = ""
             tempArray = []
-            for i in self.rhythmArray:
+            for i in self.rhythmList:
                 if i != "/":
                     temp += i
                 else:
                     tempArray.append(temp)
                     temp = ""
             tempArray.reverse()
-            self.rhythmArray = tempArray
-            print(self.rhythmArray)
+            self.rhythmList = tempArray
+            print(self.timeSig)
+            print(self.rhythmList)
+        
+    def _getTimeSig(self, w: str) -> str:
+        i = 0
+        timeSig = ''
+        write = False
+        loop = True
+        while loop:
+            if w[i] == '>':
+                loop = False
+                write = False
+            if write:
+                timeSig += w[i]
+            if w[i] == '<':
+                write = True
+            i+=1
+        for j in range(len(timeSig)):
+            if ord(timeSig[j]) != 34:
+                self.timeSig += timeSig[j]
+        w = w[i:]
+        return w
+
 
 rhythm = CreateRhythm()
 rhythm.GetMusic()
