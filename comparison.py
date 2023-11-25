efbffc53b6da3ba0485983849ec94ab243866bc2
LenBen12345 <lennyschwarz06@gmail.com>
2023-11-25 17:33:27 +0000

    Begun comparing time differences array

--- comparison.py
+++ comparison.py
@@ -1,18 +1,12 @@
 import librosa, librosa.display
 import matplotlib.pyplot as plot
+import numpy as np
 
 from RecordAndPlay import Audio
+from CreateRhythm import CreateRhythm
 
 audio = Audio()
-
-# audio, sampleRate = librosa.load("voice.wav")
-# beatTimes = librosa.onset.onset_detect(y=audio, sr=sampleRate, units="time")
-
-# plot.figure(figsize=(10,3))
-# plot.ylim(-1,1)
-# librosa.display.waveshow(audio, sr=sampleRate)
-# plot.vlines(beatTimes, -1, 1, colors="r")
-# plot.show()
+cr = CreateRhythm()
 
 class getDifference:
     def __init__(self) ->  None:
@@ -32,10 +26,17 @@ class getDifference:
         plot.vlines(beatTimes, -1, 1, colors="r")
         plot.show()
 
-    def getDifferenfce(self):
+    def getDifference(self):
         audio, beatTimes = self._getAudioArray()
-        for i in range(len(beatTimes)):
+        beatTimes.tolist()
+        for i in range(len(beatTimes)-1):
+            self.timeDiffs.append(beatTimes[i+1] - beatTimes[i])
+        print(self.timeDiffs)
+        cr.GetMusic()
+        cr._calculateBeatLength()
+        print(cr.beatLength)
+        print(cr.lengthArray)
             
 
 getDiff = getDifference()
-# getDiff.plotGraph()
+getDiff.getDifference()
