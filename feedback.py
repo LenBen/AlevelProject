c887b8bd88af95ca9bb483869b04d89fc61c98ef
Lenny <lennyschwarz06@gmail.com>
2023-11-28 15:26:16 +0000

    Added more to the feedback class

--- feedback.py
+++ feedback.py
@@ -1,11 +1,28 @@
 
 class feedback():
     def __init__(self) -> None:
-        self.feedbackValues = []
-        self.fValues = [1,2,3]
+        self.feedbackValues : list = []
+        self.fValues : list = [1,2,3]
+        self.messages : list = []
+        self.score = 0
     
-    def setFeedbackValues(self, fVal : list):
+    def setFeedbackValues(self, fVal : list) -> None:
         for i in range(len(fVal)):
             if not(fVal[i] in self.fValues):
                 return
         self.feedbackValues = fVal
+    
+    def giveFeedback(self) -> None:
+        for i in range(len(self.feedbackValues)):
+            match i:
+                case 1:
+                    self.messages.append("Beat in time")
+                case 2:
+                    self.messages.append("Beat slightly off time")
+                case 3:
+                    self.messages.append("Beat off time")
+        x = 0
+        for i in range(len(self.feedbackValues)):
+            match i:
+                case 1:
+                    x += 1 / len(self.feedbackValues)
