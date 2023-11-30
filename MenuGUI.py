0109ca0e34a15edb2c6ae1a3a29b1d2831443761
Lenny <lennyschwarz06@gmail.com>
2023-11-30 14:21:54 +0000

    Made a start screen GUI

--- MenuGUI.py
+++ MenuGUI.py
@@ -30,8 +30,39 @@ class MenuScreen(QWidget):
         super().__init__()
         self.windowSize = 512
         self.displaySize = 40
-        self.buttonSize = 60
         self._initialiseUI()
 
     def _initialiseUI(self):
-        self.setWindowIcon()
+        self.setFixedSize(self.windowSize, self.windowSize)
+        self.setWindowTitle("The Rhythm Checker")
+        self.setWindowIcon(QIcon("appLogo.png"))
+        self._setUpMainWindow()
+        self.show()
+    
+    def _setUpMainWindow(self):
+        self._createLabels()
+        self._createButton()
+    
+    def _createLabels(self):
+        title_label = QLabel(self)
+        title_label.setText("The Rhythm Checker")
+        title_label.setFont(QFont("Arial", 35))
+        title_label.move(self.windowSize//12,30)
+
+        desc_label = QLabel(self)
+        desc_label.setText("Input sheet music and compare with your playing!")
+        desc_label.setFont(QFont("Arial",10))
+        desc_label.move(self.windowSize//4, 95)
+
+    def _createButton(self):
+        self.startButton = QPushButton("Start the rhythm checker!",self)
+        self.startButton.setFixedSize(400, 200)
+        self.startButton.move(self.windowSize//8,self.windowSize//3)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MenuScreen()
+    sys.exit(app.exec())
+
+if __name__ =="__main__":
+    main()
