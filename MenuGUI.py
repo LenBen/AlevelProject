0212de39c0e5cf4c0ed3b6313fe8ba3f0c8d6024
LenBen12345 <lennyschwarz06@gmail.com>
2024-01-05 16:43:16 +0000

    comments added to menu and step1

--- MenuGUI.py
+++ MenuGUI.py
@@ -1,4 +1,4 @@
-import sys
+import sys                      #Importing all of the libraries and classes used in the program
 import typing
 from PyQt6 import QtCore
 
@@ -34,18 +34,18 @@ class MenuScreen(QWidget):
         self.displaySize = 40
         self._initialiseUI()
 
-    def _initialiseUI(self):
+    def _initialiseUI(self):        # Initialise the size, title and icon of the window.
         self.setFixedSize(self.windowSize, self.windowSize)
         self.setWindowTitle("The Rhythm Checker")
         self.setWindowIcon(QIcon("Images\\appLogo.png"))
         self._setUpMainWindow()
         self.show()
     
-    def _setUpMainWindow(self):
+    def _setUpMainWindow(self):     # Calls methods to set up the components of the window
         self._createLabels()
         self._createButton()
     
-    def _createLabels(self):
+    def _createLabels(self):        # Creates all the text labels in the program
         title_label = QLabel(self)
         title_label.setText("The Rhythm Checker")
         title_label.setFont(QFont("Arial", 35))
@@ -61,21 +61,19 @@ class MenuScreen(QWidget):
         dec2_label.setFont(QFont("Arial",8))
         dec2_label.move(self.windowSize//4,115)
 
-        button_label = QLabel
-
-    def _createButton(self):
+    def _createButton(self):        # Creates the button of the window
         self.startButton = QPushButton("Start the rhythm checker!",self)
         self.startButton.setFixedSize(400, 200)
         self.startButton.setFont(QFont("Arial", 20))
         self.startButton.move(self.windowSize//8,self.windowSize//3)
         self.startButton.clicked.connect(self._callOtherPage)
 
-    def _callOtherPage(self):
+    def _callOtherPage(self):       # Calls the next page of the program
         self.window = step1Window()
         # self.window = MainWindow()
         self.window.show()
 
-def main():
+def main():     # Function if the file is run by itself
     app = QApplication(sys.argv)
     window = MenuScreen()
     sys.exit(app.exec())
--- step1.py
+++ step1.py
@@ -1,4 +1,4 @@
-import sys
+import sys                  # Import all the libraries and classes for the program
 import typing
 from PyQt6 import QtCore
 
@@ -32,24 +32,24 @@ class step1Window(QWidget):
         self.uploadedImage = False
         self._initialiseUI()
     
-    def _initialiseUI(self):
+    def _initialiseUI(self):        # Initialise the size, title and icon of the window.
         self.setFixedSize(self.windowSize, self.windowSize)
         self.setWindowTitle("The Rhythm Checker")
         self.setWindowIcon(QIcon("Images\\appLogo.png"))
         self._setUpMainWindow()
         self.show()
     
-    def _setUpMainWindow(self):
+    def _setUpMainWindow(self):     # Calls methods to set up the components of the window
         self._createLabels()
         self._createButtons()
     
-    def _createLabels(self):
+    def _createLabels(self):        # Creates all the text labels in the program
         title_label = QLabel(self)
         title_label.setText("Step 1")
         title_label.setFont(QFont("Arial", 35))
         title_label.move(self.windowSize//3,30)
     
-    def _createButtons(self):
+    def _createButtons(self):       # Creates the button of the window
         self.nextButton = QPushButton("Next", self)
         self.nextButton.setFixedSize(100,50)
         self.nextButton.move(400,450)
@@ -62,7 +62,7 @@ class step1Window(QWidget):
         self.uploadButton.move(self.windowSize//8, self.windowSize//4)
         self.uploadButton.clicked.connect(self._uploadFile)
     
-    def _callNextPage(self):
+    def _callNextPage(self):        # Calls the next class if an image has been uploaded
         if self.uploadedImage:
             self.window = step2Window()
             self.window.show()
@@ -72,19 +72,21 @@ class step1Window(QWidget):
                                  <p>Please upload a file</p>""",
                                  QMessageBox.StandardButton.Ok)
     
-    def _uploadFile(self):
-        rhythmFile = QFileDialog.getOpenFileUrl(self, "Open Rhythm File",
+    def _uploadFile(self):      # Uploads the file
+        rhythmFile = QFileDialog.getOpenFileUrl(self, "Open Rhythm File",       # Opens the file via the path of the file
                                                 QUrl("C://"), "Image Files(*.jpg *.jpeg *.png)")
         
-
-        image = Image.open(QUrl.toLocalFile(rhythmFile[0]))
-        image = image.save("O_Input\\music.png")
+        try:
+            image = Image.open(QUrl.toLocalFile(rhythmFile[0]))
+            image = image.save("O_Input\\music.png")        # Saves the file 
+        except Exception as e:
+            print(e)
 
         self.uploadedImage = True
     
 
         
-def main():
+def main():         # Function if the file is run by itself
     app = QApplication(sys.argv)
     window = step1Window()
     sys.exit(app.exec())
